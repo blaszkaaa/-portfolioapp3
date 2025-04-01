f4d0ab57b5cf903976315ee7e9bda66ebbd6e7f3
Blaszkaaa <mateuszniema1@gmail.com>
2025-04-01 21:38:33 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/portfolioapp3">
+      <BrowserRouter basename="/-portfolioapp3">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
