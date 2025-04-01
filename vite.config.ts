d8b4e80d1feff70dd9acba66fbe0cc8e1941f917
Blaszkaaa <mateuszniema1@gmail.com>
2025-04-01 21:37:35 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/portfolioapp3/', // Nazwa repozytorium
+  base: '/-portfolioapp3/', // Nazwa repozytorium
   plugins: [react()],
   resolve: {
     alias: {
